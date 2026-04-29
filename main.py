ab9d7d34d3d0843eab5041766a569cb90199c417
yyerali <esenbaeverali@gmail.com>
2026-04-29 11:34:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1",
-    api_key="########"
+    api_key=os.getenv("MY_API_KEY")
 )
 
 app = Flask(__name__)
